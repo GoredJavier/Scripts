addc7f3f7ebdeb9466390388647dfb5743929796
User <parrot@localhost.localdomain>
2023-09-30 18:30:42 +0100

    ADD:FORMAT -> added calculateMask function and formated style and fucntions

--- Ciberseguridad/scanHosts.sh
+++ Ciberseguridad/scanHosts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# CTRL C EXIT GESTION
+
 trap ctrl_c SIGINT
 
 function ctrl_c(){
@@ -8,8 +10,15 @@ function ctrl_c(){
   exit 1
 }
 
+# GLOBAL VARIABLES
+
 declare -a ip
 declare -g binIP
+binMask0=""
+binMask1=""
+binMask=""
+
+# FUNCTIONS
 
 function parseIP(){
   for i in $(seq 1 4); do
@@ -19,6 +28,35 @@ function parseIP(){
   binIP="${ip[1]}${ip[2]}${ip[3]}${ip[4]}"
 }
 
+function calculateBaseIP(){
+  echo -e "\nVamos a calcular la IP base de $1 con la mask $binMask"
+}
+
+function calculateMask(){
+  decimalMask0=$1
+  decimalMask1=$2
+  zeroBit="0"
+  oneBit="1"
+  for ((i=0; i<decimalMask0; i++)); do
+    binMask0="$binMask0$zeroBit"
+  done
+  for ((i=0; i<decimalMask1; i++)); do
+    binMask1="$binMask1$oneBit"
+  done
+  binMask="$binMask1$binMask0"
+}
+
+# MAIN
+
+tput civis
+
 parseIP $1
 
-echo "$binIP"
+cidr=$2
+mask=$((32 - cidr))
+
+calculateMask $mask $cidr
+
+calculateBaseIP $binIP
+
+tput cnorm
