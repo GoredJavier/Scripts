38fc3110568675ab12966456e252a662fc1003c5
User <parrot@localhost.localdomain>
2023-10-02 21:14:20 +0100

    ADD -> Added the function for lastIP calculation

--- Ciberseguridad/scanHosts.sh
+++ Ciberseguridad/scanHosts.sh
@@ -15,8 +15,15 @@ function ctrl_c(){
 declare -a ip
 declare -g binIP
 binMask0=""
+binMask0aux=""
 binMask1=""
+binMask1aux=""
 binMask=""
+binMaskaux=""
+cidr=$2
+mask=$((32 - cidr))
+baseIP=""
+lastIP=""
 
 # FUNCTIONS
 
@@ -29,21 +36,43 @@ function parseIP(){
 }
 
 function calculateBaseIP(){
-  echo -e "\nVamos a calcular la IP base de $1 con la mask $binMask"
+  echo -e "\nVamos a calcular la IP base de $binIP con la mask $binMask"
+  decimalMask=$(echo "ibase=2; $binMask" | bc)
+  decimalIP=$(echo "ibase=2; $binIP" | bc)
+  decimalBaseIP=$(((decimalIP & decimalMask) + 1))
+  baseIP=$(echo "obase=2; $decimalBaseIP" | bc)
 }
 
-function calculateMask(){
-  decimalMask0=$1
-  decimalMask1=$2
+function calculateMasks(){
+  decimalMask0=$mask
+  decimalMask1=$cidr
   zeroBit="0"
   oneBit="1"
   for ((i=0; i<decimalMask0; i++)); do
     binMask0="$binMask0$zeroBit"
+    binMask1aux="$binMask1aux$oneBit"
   done
   for ((i=0; i<decimalMask1; i++)); do
     binMask1="$binMask1$oneBit"
+    binMask0aux="$binMask0aux$zeroBit"
   done
   binMask="$binMask1$binMask0"
+  binMaskaux="$binMask0aux$binMask1aux"
+}
+
+function calculateLastIP(){
+  echo -e "\nVamos a calcular la ultima IP"
+  size=${#baseIP}
+  size=$((size-1))
+  for ((i=0; i<$size; ++i));do
+    if [ "${baseIP:i:1}" == "1" ] || [ "${binMaskaux:i:1}" == "1" ]; then
+      lastIP+="1"
+    else
+      lastIP+="0"
+    fi
+  done
+  lastIP+="0"
+  echo "$lastIP"
 }
 
 # MAIN
@@ -52,11 +81,10 @@ tput civis
 
 parseIP $1
 
-cidr=$2
-mask=$((32 - cidr))
+calculateMasks
 
-calculateMask $mask $cidr
+calculateBaseIP
 
-calculateBaseIP $binIP
+calculateLastIP
 
 tput cnorm
